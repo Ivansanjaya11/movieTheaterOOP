1a733b4ee1f4d7ab8eeac728160a70dbb35b519a
LoganAMC <logan.cordova@enmu.edu>
2025-03-25 19:35:48 -0600

    Logan updated Customer.java

--- stage3/Customer.java
+++ stage3/Customer.java
@@ -1,4 +1,5 @@
-/**
+/** This class represents a customer and stores their name, email, and phone number
+ * Allows for access and modification to be made to customer details
  * @author Logan Cordova
  */
 
@@ -8,37 +9,73 @@ public class Customer {
     private String email;
     private String phoneNumber;
 
-    //Should void be added to this constructor???
-     public void Customer(String name) {
+    /**
+     * Constructor initializing customer with name
+     * @param name
+     */
+     public Customer(String name) {
         this.name = name;
      }
 
-     public void Customer(String name, String email, String phoneNumber) {
+     /**
+      * Constructor initializing name, email address, and phone number of customer
+      * @param name
+      * @param email
+      * @param phoneNumber
+      */
+     public Customer(String name, String email, String phoneNumber) {
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
      }
 
+     /**
+      * Returns name of customer
+      * @return
+      */
      public String getName() {
         return name;
      }
 
+     /**
+      * Returns email of customer
+      * @return
+      */
      public String getEmail() {
         return email;
      }
 
+     /**
+      * Returns phone number of customer
+      * @return
+      */
      public String getPhoneNumber() {
         return phoneNumber;
      }
 
+     /**
+      * Updates customer name
+      * Sets new customer name
+      * @param name
+      */
      public void setName(String name) {
         this.name = name;
      }
 
+     /**
+      * Updates customer email
+      * Sets new customer email
+      * @param email
+      */
      public void setEmail(String email) {
         this.email = email;
      }
 
+     /**
+      * Updates customer phone number
+      * Sets new customer phone number 
+      * @param phoneNumber
+      */
      public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
      }
