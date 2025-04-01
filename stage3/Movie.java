27f88e93626ba819e8b7e4cf756ddc25b0816afe
LoganAMC <logan.cordova@enmu.edu>
2025-03-31 21:46:56 -0600

    Logan update Movie class

--- stage3/Movie.java
+++ stage3/Movie.java
@@ -3,13 +3,19 @@
  */
 
 public class Movie {
-    //Movie details
-    private byte movieID
-    private String title
-    private String genre
-    private short durationMinutes
 
-    //Constructor initializes Movie object
+    private byte movieID;
+    private String title;
+    private String genre;
+    private short durationMinutes;
+
+    /**
+     * Constructor initializes Movie object
+     * @param movieID - unique movie ID
+     * @param title - moive title
+     * @param genre - movie genre
+     * @param durationMinutes - duration of move
+     */
     public Movie(byte movieID, String title, String genre, short durationMinutes) {
 
         this.movieID = movieID;
@@ -18,37 +24,58 @@ public class Movie {
         this.durationMinutes = durationMinutes;
     }
 
-    //Retruns movie ID
+    /**
+     * Returns movie ID
+     * @return
+     */
     public byte getMovieID() {
         return movieID;
     }
 
-    //Returns movie title
+    /**
+     * Returns movie title
+     * @return
+     */
     public String getTitle() {
         return title;
     }
  
-    //Returns movie genre
+    /**
+     * Returns movie genre
+     * @return
+     */
     public String getGenre() {
         return genre;
     }
 
-    //Returns movie duriation in minutes
+    /**
+     * Returns movie duration
+     * @return
+     */
     public short getDurationMinutes() {
         return durationMinutes;
     }
 
-    //Sets movie title
+    /**
+     * Sets movie title
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    //Sets movie genre
+    /**
+     * Sets movie genre
+     * @param genre
+     */
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
-    //Sets movie duration in minutes
+    /**
+     * Sets movie duration, in minutes
+     * @param durationMinutes
+     */
     public void setDurationMinutes(short durationMinutes) {
         this.durationMinutes = durationMinutes;
     }
